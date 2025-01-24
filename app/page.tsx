8d503e9c592141a30f38f9eb443e70d6a5fc008c
Mohtasham Murshid <mohtashammurshid@gmail.com>
2025-01-24 16:12:12 +0530

    edited footer

--- app/page.tsx
+++ app/page.tsx
@@ -79,9 +79,18 @@ export default function Page() {
           </Link>
         ))}
       </div>
-      <footer className="p-4 ">
+      <footer className="p-4 text-gray-600">
         <p className="text-center ">
           &copy; {new Date().getFullYear()} Bad Reads
+        </p>
+        <p>
+          Developed by{" "}
+          <a
+            href="https://mohtasham.pages.dev"
+            className="underline hover:scale-105 hover:text-gray-300"
+          >
+            Mohtasham
+          </a>
         </p>
       </footer>
     </div>
